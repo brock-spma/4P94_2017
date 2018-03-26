15230a12ef7629cd02a3d929dfe2b02801464376
Kevin Mongeon <kmongeon@brocku.ca>
2018-03-26 16:22:08 -0400

    NFL win prob model

---
+++ 3-tests/2-test2/1-prep/2-ingame_win_prob/_imports.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import pandas
+import numpy
+import datetime, time
+
+import matplotlib.pyplot as plt
+%matplotlib notebook
+
+import statsmodels.api as sm
+from pylab import hist, show
+import scipy
+import statsmodels.api as sm
+
+pandas.set_option("display.max_rows", 35)
+pandas.set_option("display.max_columns", 50)
+pandas.set_option("display.max_colwidth", 200)
+
